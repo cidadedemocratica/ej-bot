77b3439b2907dde378a5f445668dd2588809dc58
Martha <marthadntas@gmail.com>
2021-02-08 21:05:02 -0300

    create test of action setupConversation and askVote
    Signed-off-by: Martha <marthadntas@gmail.com>
    Co-authored-by: fabiola <fabiola.mfleury@gmail.com>

---
+++ bot/tests/test_actions.py
@@ -0,0 +1,42 @@
+import pytest
+
+from typing import Text, List, Any, Dict
+
+from actions import ActionSetupConversation, ActionAskVote
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet, FollowupAction
+from rasa.shared.core.trackers import DialogueStateTracker
+
+
+EMPTY_TRACKER = None
+
+@pytest.fixture
+def dispatcher():
+  return CollectingDispatcher()
+
+@pytest.fixture
+def domain():
+  return dict()
+
+def test_action_setup_conversation(dispatcher, domain):
+  tracker = EMPTY_TRACKER
+  action = ActionSetupConversation()
+  events = action.run(dispatcher, tracker, domain)
+  expected_events = [
+            SlotSet("number_voted_comments", 1),
+            SlotSet("conversation_id", 1),
+            SlotSet("number_comments", 3),
+            SlotSet("comment_text", "Comment text here"),
+            SlotSet("current_comment_id", 53),
+            SlotSet("change_comment", False),
+            FollowupAction("vote_form")
+        ]
+  assert events == expected_events
+
+def test_action_ask_vote(dispatcher, domain):
+  tracker = DialogueStateTracker.from_dict(sender_id="1", events_as_dict=List[Dict["action": 1]], slots=[List[change_comment],False])
+  action = ActionAskVote()
+  events = action.run(dispatcher, tracker, domain)
+  expected_events = [SlotSet("change_comment", True)]
+
+  assert events == expected_events
